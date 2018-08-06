80def5a4c19f16d869ac206b049bc446e88149b7
Simon Bramble <simonbramble.digitaldwp@gmail.com>
2018-08-06 13:15:42 +0100

    death arrears and FEP content tweaks

--- inquirer/eligibility.js
+++ inquirer/eligibility.js
@@ -5,7 +5,7 @@ const gutil = require('gulp-util');
 const Rx = require('rxjs');
 const c = gutil.colors;
 
-console.info(c.bold.bgMagenta('\nFuneral Expense Payment:'));
+console.info(c.bold.bgMagenta('\nFuneral Expenses Payment:'));
 inquirer.prompt(questions).then(answers => {
   eligiblity.logEligibility();
 });
